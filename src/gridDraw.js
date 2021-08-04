30571cb8dcb476168231c2c6d270a53d7e00cd58
ToBinio <Tobias.frischmann1@gmail.com>
2021-08-04 15:39:45 +0200

    better drawing

--- src/gridDraw.js
+++ src/gridDraw.js
@@ -3,16 +3,34 @@ import { cellSize } from "./options.js";
 import { gridObj } from "./script.js";
 gridObj.gridArray[0][0] = state.alive;
 gridObj.draw();
+var mode = state.dead;
+var drawing = false;
 gridObj.canvas.onmousedown = function (e) {
     var pos = {
         row: Math.round((e.offsetY - cellSize / 2) / cellSize),
         column: Math.round((e.offsetX - cellSize / 2) / cellSize),
     };
     if (gridObj.gridArray[pos.row][pos.column] == state.alive) {
-        gridObj.gridArray[pos.row][pos.column] = state.dead;
+        mode = state.dead;
     }
-    else {
-        gridObj.gridArray[pos.row][pos.column] = state.alive;
-    }
-    gridObj.draw();
+    else
+        mode = state.alive;
+    draw(mode, pos.row, pos.column);
+    drawing = true;
 };
+window.onmouseup = function (e) {
+    drawing = false;
+};
+gridObj.canvas.onmousemove = function (e) {
+    if (!drawing)
+        return;
+    var pos = {
+        row: Math.round((e.offsetY - cellSize / 2) / cellSize),
+        column: Math.round((e.offsetX - cellSize / 2) / cellSize),
+    };
+    draw(mode, pos.row, pos.column);
+};
+function draw(mode, row, column) {
+    gridObj.gridArray[row][column] = mode;
+    gridObj.draw();
+}
--- src/gridDraw.ts
+++ src/gridDraw.ts
@@ -1,9 +1,13 @@
-import { state } from "./grid.js";
+import { grid, state } from "./grid.js";
 import { cellSize } from "./options.js";
 import { gridObj } from "./script.js";
 
 gridObj.gridArray[0][0] = state.alive;
 gridObj.draw();
+
+let mode = state.dead;
+
+let drawing = false;
 
 gridObj.canvas.onmousedown = function (e) {
   let pos = {
@@ -12,10 +16,30 @@ gridObj.canvas.onmousedown = function (e) {
   };
 
   if (gridObj.gridArray[pos.row][pos.column] == state.alive) {
-    gridObj.gridArray[pos.row][pos.column] = state.dead;
-  } else {
-    gridObj.gridArray[pos.row][pos.column] = state.alive;
-  }
+    mode = state.dead;
+  } else mode = state.alive;
+
+  draw(mode, pos.row, pos.column);
+
+  drawing = true;
+};
+
+window.onmouseup = function (e) {
+  drawing = false;
+};
+
+gridObj.canvas.onmousemove = function (e) {
+  if (!drawing) return;
+  let pos = {
+    row: Math.round((e.offsetY - cellSize / 2) / cellSize),
+    column: Math.round((e.offsetX - cellSize / 2) / cellSize),
+  };
+
+  draw(mode, pos.row, pos.column);
+};
+
+function draw(mode: state, row: number, column: number) {
+  gridObj.gridArray[row][column] = mode;
 
   gridObj.draw();
-};
+}
